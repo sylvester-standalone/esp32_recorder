6112591664056da32294a1df723517c3fd25ba7d
reiji <r.nakanaka0121@gmail.com>
2025-04-07 21:58:46 +0900

    add comment

--- lib/config/pin_config.h
+++ lib/config/pin_config.h
@@ -1,9 +1,9 @@
 #pragma once
 
-/* pin設定 */
+/* pin configuration */
 // VSPI pin
 #define VSPI_SCK               GPIO_NUM_18
-#define VSPI_MOSI              GPIO_NUM_21         // default -> 23
+#define VSPI_MOSI              GPIO_NUM_21         // default -> GPIO23
 #define VSPI_MISO              GPIO_NUM_19
 #define VSPI_CS                GPIO_NUM_5
 
@@ -15,9 +15,9 @@
 #define I2S_STD_DIN            GPIO_NUM_26         // I2S data in io number
 
 // I2C pin
-#define OLED_SDA               GPIO_NUM_22         // default SDA:GPIO21
-#define OLED_SCL               GPIO_NUM_23         // default SCL:GPIO22
+#define OLED_SDA               GPIO_NUM_22         // default -> GPIO21
+#define OLED_SCL               GPIO_NUM_23         // default -> GPIO22
 
 // Button pin
-#define BTN_REC_PLAY           GPIO_NUM_34         // rec/playボタンピン(3.3V, pulldown)
-#define BTN_MODE               GPIO_NUM_35         // mode切り替えボタンピン(3.3V, pulldown)
+#define BTN_REC_PLAY           GPIO_NUM_34         // rec/play Button(3.3V, pulldown)
+#define BTN_MODE               GPIO_NUM_35         // mode Button(3.3V, pulldown)
